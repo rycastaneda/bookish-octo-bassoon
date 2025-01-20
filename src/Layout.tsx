56278d2723fac2ee78223f57541e2c76f57f9dd5
Ryan Castañeda <castaneda.ry@gmail.com>
2025-01-20 17:45:17 +0800

    remove logo

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react'
-import sesimi from '/sesimi.svg'
 
 type LayoutProps = {
   children: ReactNode
@@ -10,14 +9,7 @@ const Layout = ({ children, complementary }: LayoutProps) => {
   return (
     <div className={`grid items-center`}>
       <main>
-        <div>
-          <img
-            src={sesimi}
-            className="logo mx-auto"
-            alt="Vite logo"
-            width="140"
-          />
-        </div>
+
         <h1>todo…</h1>
         <div className="card">{children}</div>
       </main>
